fa31fa98ac8ae52af63d4de2a0e0db8e3f06dec5
erenzhou <592267829@qq.com>
2021-12-21 15:52:08 +0800

    Update __init__.py

--- r3det/datasets/__init__.py
+++ r3det/datasets/__init__.py
@@ -1,5 +1,4 @@
 from .dota1 import DOTADataset  # noqa: F401, F403
 from .pipelines import *  # noqa: F401, F403
-from .rotate import ROTATEDataset  # noqa: F401, F403
 
-__all__ = ['ROTATEDataset', 'DOTADataset']
+__all__ = ['DOTADataset']
